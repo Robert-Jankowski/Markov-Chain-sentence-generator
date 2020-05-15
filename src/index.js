35100290e66f6aee0b59926a1d3f7969922108ad
Robert <robert.jankowski.kontakt@gmail.com>
2020-05-15 16:21:00 +0200

    Algorithm working, but 'null' issue

--- src/index.js
+++ src/index.js
@@ -7,10 +7,39 @@ function fileToArray() {
     const text = fs.readFileSync("../data/text.txt", "utf-8")
         .split(" ");
     const dotToNone = _.map(text, function (n) {
-        return (n=== ".") ? null : n;
+        return (n== ".") ? null : n;
     })
-    return dotToNone;
+    const removeUndefined = _.filter(dotToNone, (n) => n !== "" && n != undefined);
+    return removeUndefined;
+}
+
+function buildChain(listOfObjects,data) {
+     _.forEach(data, (n,i) => {
+         const current = _.findIndex(listOfObjects, {key : n});
+         const following = _.findIndex(listOfObjects, {key: data[i+1]});
+         listOfObjects[current].next = [...listOfObjects[current].next, listOfObjects[following]];
+    });
+}
+
+function Word(key) {
+    this.key = key;
+    this.next = [];
 }
 
 const data = fileToArray();
-const noDuplicates = [... new Set(data)].filter((n) => (n!== null));
+const noDuplicates = [... new Set(data)];
+
+const listOfObjects = noDuplicates.map((n) =>  n = new Word(n));
+
+buildChain(listOfObjects,data);
+
+function makeSentence(node, sentence = "") {
+    console.log("key",node.key);
+    const takeRandom = node.next[Math.floor(Math.random() * node.next.length)];
+    console.log("next",takeRandom.key);
+    return (node.key == null ) ? sentence : makeSentence(takeRandom, (`${sentence} ${node.key}`));
+}
+
+// const sentence1 = makeSentence(listOfObjects[123]);
+// console.log(sentence1);
+console.log(_.find(listOfObjects, {key: null}));
